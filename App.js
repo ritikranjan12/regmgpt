24026d06c36f1628f9b22c04e051ec648550b97f
ritikranjan12 <ritik123453@gmail.com>
2024-02-11 14:28:01 +0530

    version 1.0.0 published

--- App.js
+++ App.js
@@ -1,35 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Pressable, Image, useColorScheme } from 'react-native';
-import * as LocalAuthentication from 'expo-local-authentication';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import {Textmodel, Chatmodel, Multimodel} from './models'
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  useColorScheme,
+  TextInput,
+  Animated,
+} from "react-native";
+import * as LocalAuthentication from "expo-local-authentication";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { Textmodel, Chatmodel, Multimodel } from "./models";
+import { ScrollView } from "react-native";
+
 const Tab = createBottomTabNavigator();
 
-
-const handleTextOutput = async() => {
-  const prompt = "Write a story about a magic backpack."
-
-  const result = await Textmodel.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
-
 const TextInputScreen = () => {
+  const [textInput, setTextInput] = useState("");
+  const [textOutput, setTextOutput] = useState("");
+  const [animation] = useState(new Animated.Value(0));
+
+  const handleTextOutput = async (prompt, setTextInput, setTextOutput) => {
+    setTextOutput("Loading ....");
+    try {
+      const result = await Textmodel.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      setTextOutput(text);
+      setTextInput("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const startAnimation = () => {
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const animatedStyles = {
+    opacity: animation,
+    transform: [
+      {
+        translateY: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [100, 0],
+        }),
+      },
+    ],
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Text Input Screen</Text>
-      <Pressable onPress={handleTextOutput}></Pressable>
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Text Input Screen</Text>
+        <TextInput
+          style={styles.textInput}
+          value={textInput}
+          onChangeText={(e) => setTextInput(e)}
+          placeholder="Enter prompt"
+          multiline={true}
+        />
+        <Pressable
+          onPress={() => {
+            handleTextOutput(textInput, setTextInput, setTextOutput);
+            startAnimation();
+          }}
+        >
+          <Text style={styles.button}>Submit</Text>
+        </Pressable>
+        <Animated.View style={[styles.outputContainer, animatedStyles]}>
+          <Text style={styles.outputText}>{textOutput}</Text>
+        </Animated.View>
+      </View>
+    </ScrollView>
   );
 };
 
 const ChatScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Chat Screen</Text>
+      <Text style={styles.text}>Coming Soon ...</Text>
     </View>
   );
 };
@@ -37,7 +93,7 @@ const ChatScreen = () => {
 const LongChatScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Long Chat Screen</Text>
+      <Text style={styles.text}>Coming Soon ....</Text>
       {/* Add your long chat logic here */}
     </View>
   );
@@ -45,10 +101,10 @@ const LongChatScreen = () => {
 
 const App = () => {
   const colorScheme = useColorScheme();
-  const isDarkMode = colorScheme === 'dark';
+  const isDarkMode = colorScheme === "dark";
 
   useEffect(() => {
-    authenticate();
+    // authenticate();
   }, []);
 
   const authenticate = async () => {
@@ -58,65 +114,64 @@ const App = () => {
       if (isEnrolled) {
         const result = await LocalAuthentication.authenticateAsync();
         if (result.success) {
-          console.log('Biometric authentication successful');
+          console.log("Biometric authentication successful");
         } else {
-          authenticate()
+          authenticate();
         }
       } else {
-        authenticate()
+        authenticate();
       }
     } else {
-      console.log('Biometric hardware not available');
+      console.log("Biometric hardware not available");
     }
   };
-  
 
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarStyle: {
-            backgroundColor: isDarkMode ? '#333' : '#fff',
+            backgroundColor: isDarkMode ? "#333" : "#fff",
           },
-          tabBarActiveTintColor: isDarkMode ? '#fff' : '#000',
-          tabBarInactiveTintColor: isDarkMode ? '#999' : '#666',
+          tabBarActiveTintColor: isDarkMode ? "#fff" : "#000",
+          tabBarInactiveTintColor: isDarkMode ? "#999" : "#666",
           headerStyle: {
-            backgroundColor: isDarkMode ? '#333' : '#fff',
+            backgroundColor: isDarkMode ? "#333" : "#fff",
           },
-          headerTintColor: isDarkMode ? 'gray' : '#000',
+          headerTintColor: isDarkMode ? "white" : "black",
           tabBarLabelStyle: {
             fontSize: 16,
-            fontWeight: 'bold',
-            width: '33.33%', // Divide the navigation bar into three parts
-            textAlign: 'center', // Center align the tab labels
+            fontWeight: "bold",
+            width: "33.33%", // Divide the navigation bar into three parts
+            textAlign: "center", // Center align the tab labels
           },
           tabBarIndicatorStyle: {
-            backgroundColor: '#ff0000',
+            backgroundColor: "#ff0000",
             height: 3,
           },
           tabBarIconStyle: {
             marginBottom: -3,
-            width: '100%', // Set the tab bar icons to full width
+            width: "100%", // Set the tab bar icons to full width
           },
           headerTitleStyle: {
             fontSize: 20,
-            fontWeight: 'bold',
+            fontWeight: "bold",
           },
           tabBarIcon: ({ color, size }) => {
             let iconName;
 
-            if (route.name === 'Text Input') {
-              iconName = 'text-outline';
-            } else if (route.name === 'Chat') {
-              iconName = 'aperture-outline';
-            } else if (route.name === 'Long Chat') {
-              iconName = 'chatbubbles-outline';
+            if (route.name === "Text Input") {
+              iconName = "text-outline";
+            } else if (route.name === "Chat") {
+              iconName = "aperture-outline";
+            } else if (route.name === "Long Chat") {
+              iconName = "chatbubbles-outline";
             }
 
             return <Icon name={iconName} size={size} color={color} />;
           },
           tabBarLabelStyle: {
-            display: 'none',
+            display: "none",
           },
         })}
       >
@@ -131,17 +186,52 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor:'black'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+    flexGrow: 1, 
   },
+  scrollContainer: { 
+    flexGrow: 1, 
+    backgroundColor: "black",
+   },
   text: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 16,
-    color: '#333', // Set the default text color
+    color: "white", // Set the default text color
   },
-  // Add additional styles for dark mode here
+  textInput: {
+    width: "60%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 16,
+    color: "white",
+  },
+  button: {
+    fontSize: 18,
+    fontWeight: "medium",
+    color: "white",
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+    opacity: 0.89,
+    shadowColor: "red",
+    boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+  },
+  outputContainer: {
+    marginTop: 16,
+    padding: 10,
+    backgroundColor: "gray",
+    borderRadius: 5,
+  },
+  outputText: {
+    fontSize: 16,
+    color: "white",
+  },
 });
 
 export default App;
--- models.js
+++ models.js
@@ -1,6 +1,8 @@
-import { GoogleGenerativeAI } from "google-generative-ai";
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import Constants from "expo-constants";
 
-export const Textmodel = genAI.getGenerativeModel({ model: "gemini-pro"})
-export const Multimodel = genAI.getGenerativeModel({ model: "MODEL_NAME"});
-export const Chatmodel = genAI.getGenerativeModel({ model: "MODEL_NAME"});
+const genAI = new GoogleGenerativeAI(process.env.EXPO_PUBLIC_API_KEY);
+
+export const Textmodel = genAI.getGenerativeModel({ model: "gemini-pro" });
+export const Multimodel = genAI.getGenerativeModel({ model: "MODEL_NAME" });
+export const Chatmodel = genAI.getGenerativeModel({ model: "MODEL_NAME" });
